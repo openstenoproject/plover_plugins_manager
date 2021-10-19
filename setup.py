679b1db11d97618adb28d58babfa10a671e9c1f3
Benoit Pierre <benoit.pierre@gmail.com>
2021-10-19 18:02:43 +0200

    setup: remove use of `plover_build_utils.setup.Test`
    Not available anymore.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools>=38.2.4
 
 from setuptools import setup
 
-from plover_build_utils.setup import BuildPy, BuildUi, Test
+from plover_build_utils.setup import BuildPy, BuildUi
 
 
 BuildPy.build_dependencies.append('build_ui')
@@ -14,7 +14,6 @@ BuildUi.hooks = ['plover_build_utils.pyqt:fix_icons']
 cmdclass = {
     'build_py': BuildPy,
     'build_ui': BuildUi,
-    'test': Test,
 }
 
 setup(cmdclass=cmdclass)
